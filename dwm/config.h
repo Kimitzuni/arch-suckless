6f97561e90feb94b881ddf6e084e0d941e2b323f
Rebecca <kimitxuni@gmail.com>
2021-04-06 15:17:05 +0100

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -1,24 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 15;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {
- "Code New Roman:size=15",
- "fontawesome:size=15",
- "Noto Sans Emoji:size=15",
-};
-static const char dmenufont[]       = "Code New Roman:size=15";
+static const char *fonts[]          = { "Cascadia Code:size=12","Font Awesome Regular:size=12","Font Awesome Brands:size=12","JoyPixels:pixelsize=15:antialias=true:autohint=true"  };
+static const char dmenufont[]       = "Cascadia Code:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-// static const char col_cyan[]        = "#d142f5";
-static const char col_cyan[]        = "#f06c6c";
-static const char col_brdr[]	    = "#333131";
+static const char col_cyan[]        = "#7A5ADA";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -26,7 +20,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "4", "5", "6" };
+static const char *tags[] = { "📁", "🖥️", "💬", "🌎", "🎵"};
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -34,7 +29,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-    { "discord",  NULL,       NULL,       2,            0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "discord",  NULL,		  NULL,		  2,			0,			 -1 },
 };
 
 /* layout(s) */
@@ -42,22 +38,16 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
-#include "fibonacci.c"
 static const Layout layouts[] = {
-        /* symbol     arrange function */
-        { "",      tile },    /* first entry is default */
-        { "",      spiral },    /* no layout function means floating behavior */
-        { "",      monocle },
-        { "[@]",      NULL },
-        { "[\\]",      dwindle },
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask // Alt Key
-#define WinKey Mod4Mask
-
-// #define MODKEY Mod4Mask // Windows Key
-
+#define MODKEY Mod1Mask
+#define WINKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -70,25 +60,21 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
-
-static const char *menu[] = { "rofi_run", NULL };
-static const char *termcmd[] = { "st", NULL };
-
-
-static const char *filemgr[] = { "pcmanfm", NULL };
-static const char *discord[] = { "discord", NULL };
-static const char *chromium[] = { "firefox", NULL }; // Beyond cursed
+/* Programs */
+static const char *fileman[] = { "pcmanfm", NULL };
+static const char *web[] = { "firefox", NULL };
+static const char *netim[] = { "discord", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-//	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, // Uncomment this line if you use dmenu
-    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-    { WinKey,                       XK_Tab,    spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ WINKEY,						XK_Tab,	   spawn,		   {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,	            XK_t,	   spawn,	       {.v = filemgr} },
-	{ MODKEY|ShiftMask,		        XK_w,	   spawn,          {.v = chromium } },
-	{ MODKEY|ShiftMask,		        XK_d,	   spawn,	       {.v = discord } },	
+	{ MODKEY|ShiftMask,				XK_d,	   spawn,		   {.v = netim } },
+	{ MODKEY|ShiftMask,				XK_w,	   spawn,		   {.v = web } },
+	{ MODKEY|ShiftMask,				XK_t,	   spawn,		   {.v = fileman } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -99,11 +85,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_x,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -124,12 +108,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-    TAGKEYS(                        XK_F1,                     0)
-    TAGKEYS(                        XK_F2,                     1)
-    TAGKEYS(                        XK_F3,                     2)
-    TAGKEYS(                        XK_F4,                     3)
-    TAGKEYS(                        XK_F5,                     4)
-    TAGKEYS(                        XK_F6,                     5)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
